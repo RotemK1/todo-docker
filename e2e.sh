cf6797879bc7482e06bff2685204264079e9bd69
Administrator <admin@example.com>
2022-08-25 16:47:43 +0300

    testing e2e

--- e2e.sh
+++ e2e.sh
@@ -9,14 +9,14 @@
 #         fi
 # done
 website="app_container:5000"
-declare -a curl_list=('localhost:5000/search?refer=curltest' 'localhost:5000/list' '-X POST localhost:5000/action?name=tat&desc=11')
+declare -a curl_list=("$website/search?refer=curltest" "$website/list" "-X POST $website/action?name=tat&desc=11")
 
 for action in "${curl_list[@]}";do
         #echo $action
         status_code=`curl -sL -w %{http_code} -I $action -o /dev/null\n | tail -1`
-        echo $action
+        
         if (($status_code >= 200 && $status_code < 400));then
-                echo $status_code
+                echo "$action status-code : $status_code"
         else
             exit 1
         fi
