c42292fa83e0b9e2b3d038b80d48be4507412db9
Administrator <admin@example.com>
2022-08-25 15:40:50 +0300

    testing e2e test script

--- Jenkinsfile
+++ Jenkinsfile
@@ -38,19 +38,12 @@ pipeline {
                 echo "#########################################"
                 script{
                             // curl list = list of curl testing with GET , POST
-                        sh  """
+                        sh  ''' #!/bin/bash
                                 docker-compose up --build -d
+                                ./e2e.sh
 
-                                declare -a curl_list=(`curl -sL -w '%{http_code}' -I app_container:5000/search?refer=curltest -o /dev/null` `curl -sL -w '%{http_code}' -I app_container:5000/list -o /dev/null` `curl -sL -w '%{http_code}' -I app_container:5000 -o /dev/null` `curl -sL -w '%{http_code}' -I -X POST app_container:5000/action?name=tat&desc=11 -o /dev/null`)
-                                for status_code in ${curl_list[@]};do
-                                        if ((\$status_code >= 200 && \$status_code < 400));then
-                                                echo \$status_code
-                                        else
-                                            exit 1
-                                        fi
-                                done
+                            '''
 
-                            """
 
                 }
             }
--- e2e.sh
+++ e2e.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
-declare -a curl_list=(`curl -sL -w "%{http_code}" -I "app_container:5000/search?refer=curltest" -o /dev/null` `curl -sL -w "%{http_code}" -I "app_container:5000/list" -o /dev/null` `curl -sL -w "%{http_code}" -I "app_container:5000" -o /dev/null` `curl -sL -w "%{http_code}" -I -X POST "app_container:5000/action?name=tat&desc=11" -o /dev/null`)
-for status_code in ${curl_list[@]};do
+# declare -a curl_list=(`curl -sL -w "%{http_code}" -I "app_container:5000/search?refer=curltest" -o /dev/null` `curl -sL -w "%{http_code}" -I "app_container:5000/list" -o /dev/null` `curl -sL -w "%{http_code}" -I "app_container:5000" -o /dev/null` `curl -sL -w "%{http_code}" -I -X POST "app_container:5000/action?name=tat&desc=11" -o /dev/null`)
+# for status_code in ${curl_list[@]};do
+#         if (($status_code >= 200 && $status_code < 400));then
+#                 echo $status_code
+#         else
+#             exit 1
+#         fi
+# done
+website="app_container:5000"
+curl_list="$website:5000/search?refer=curltest" "$website:5000/list" "-X POST $website/action?name=tat&desc=11"
+
+for action in ${curl_list[*]};do
+        status_code = `curl -sL -w '%{http_code}' -I $action -o /dev/null`
         if (($status_code >= 200 && $status_code < 400));then
                 echo $status_code
         else
