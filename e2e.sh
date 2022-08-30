09ece772b0422c933bb598d8e8a33b67dd4cfa43
Administrator <admin@example.com>
2022-08-30 09:58:34 +0300

    test

--- e2e.sh
+++ e2e.sh
@@ -3,6 +3,7 @@
 website="app_container:5000"
 declare -a curl_list=("$website/search?refer=curltest" "$website/list" "-X POST $website/action?name=tat&desc=11")
 
+
 for action in "${curl_list[@]}";do
         #echo $action
         status_code=`curl -sL -w %{http_code} -I $action -o /dev/null\n | tail -1`
